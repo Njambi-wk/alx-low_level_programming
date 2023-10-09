42d36129ad4d5a04ddbbdaac7abc534d3cfe5607
Njambi-wk <njambi.karungari04@gmail.com>
2023-10-09 03:09:29 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - print a script
+ * Return: 0 if success
+ */
+int main(void)
+{
+Printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
