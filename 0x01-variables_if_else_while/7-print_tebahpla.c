1725ef482476cda3b29676e1a0e3ff2e6e886c05
Njambi-wk <njambi.karungari04@gmail.com>
2023-10-10 02:38:36 -0700

    reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char b;
+
+	b = 'z';
+	while (b >= 'a')
+	{
+		putchar(b);
+		b--;
+	}
+	putchar('\n');
+	return (0);
+}
