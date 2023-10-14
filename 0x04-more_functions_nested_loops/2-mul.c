b132eb89cabc2aa69bee07b6478ab0b31b80fe9e
Njambi-wk <njambi.karungari04@gmail.com>
2023-10-14 14:44:03 -0700

    two int prduct

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - mupltiplies two integers
+ * @a: the first integer
+ * @b: the second int
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
