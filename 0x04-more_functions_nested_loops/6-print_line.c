1a1ae3d05fca80f62c7e2600cf2d3987afd3c2ec
Njambi-wk <njambi.karungari04@gmail.com>
2023-10-14 15:40:49 -0700

    staright line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - prints a straight line
+ * @n: numbers of times
+ * Return: always 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
