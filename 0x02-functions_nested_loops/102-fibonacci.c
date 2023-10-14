638a08d43226ba55bacedba0254fcec257ace549
Njambi-wk <njambi.karungari04@gmail.com>
2023-10-14 00:13:06 -0700

    fib

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 50;
+	unsigned long fib1 = 1;
+	unsigned long fib2 = 2;
+	unsigned long next;
+
+	printf("%lu, %lu, ", fib1, fib2);
+	for (int i = 3; i <= n; i++)
+	{
+		next = fib1 + fib2;
+		if (i == n)
+		{
+			printf("%lu\n", next);
+		}
+		else
+		{
+			printf("%lu, ", next);
+		}
+		fib1 = fib2;
+		fib2 = next;
+	}
+	return (0);
+}
+
