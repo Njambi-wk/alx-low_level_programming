f2d592efede2662fa1e81dcfe77fd8ccd6d64417
Njambi-wk <njambi.karungari04@gmail.com>
2023-10-14 16:52:12 -0700

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,7 +6,7 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size <= 0)
 	{
@@ -16,6 +16,10 @@ void print_triangle(int size)
 	{
 		for (i = 0; i <= size; i++)
 		{
+			for (k = size - i; k >= 1; k--)
+			{
+				_putchar(' ');
+			}
 			for (j = 0; j < i; j++)
 			{
 				_putchar('#');
