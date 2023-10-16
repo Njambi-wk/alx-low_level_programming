9733c1f5474f3ac5f08557c40920f17dcabae829
Njambi-wk <njambi.karungari04@gmail.com>
2023-10-16 13:34:07 -0700

    ./0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takes pointer to an int and updates value to 98
+ * @n: value to be updated
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
