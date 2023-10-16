d634fdfcb3def9558cccc3b70fee3e7bdc0abc51
Njambi-wk <njambi.karungari04@gmail.com>
2023-10-16 14:57:08 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - function hat prints reverse
+ * @s: pointer
+ * Return: the reverse and a new line
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
