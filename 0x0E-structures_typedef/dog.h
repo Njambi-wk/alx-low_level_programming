15fc183c494d1e0ac3895bc55029ee5dd4a70ae8
Njambi-wk <njambi.karungari04@gmail.com>
2023-11-15 00:44:39 -0800

    dog name,age,owner

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,8 +12,14 @@ struct dog
 	char *owner;
 	float age;
 };
+/**
+ * dog_t - struct dog typedef
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
