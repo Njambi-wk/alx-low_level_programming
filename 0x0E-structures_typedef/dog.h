4680008cc029e0beb0b96c2193237c370735b26f
Njambi-wk <njambi.karungari04@gmail.com>
2023-11-15 00:56:57 -0800

    poppy age,owner & name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,12 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG_H_
+#define DOG_H_
 /**
  * struct dog - poppys info
  * @name: name of the poppy
  * @age: poppys age
  * @owner: poppys owner
+ *
+ * description - struct dog
  */
 struct dog
 {
@@ -12,15 +14,5 @@ struct dog
 	char *owner;
 	float age;
 };
-/**
- * dog_t - struct dog typedef
- */
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
